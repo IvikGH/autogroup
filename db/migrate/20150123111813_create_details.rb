70fa7b30e6ae1b331e1ae4d07cc9f682c82ce44a
Ivik <andoidxx@gmail.com>
2015-01-23 13:56:02 +0200

    Detail migration configured

--- db/migrate/20150123111813_create_details.rb
+++ db/migrate/20150123111813_create_details.rb
@@ -1,6 +1,12 @@
 class CreateDetails < ActiveRecord::Migration
   def change
     create_table :details do |t|
+    	t.string	:brand
+    	t.string	:detail_id
+    	t.string	:detail_title
+    	t.float		:detail_price
+    	t.string	:discount_group
+    	t.float		:detail_weight
 
       t.timestamps null: false
     end
