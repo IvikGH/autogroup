507ccc23b0982767c0735b1283ed8f8a0bf9c9f0
Ivik <andoidxx@gmail.com>
2015-01-25 01:13:11 +0200

    Add some data with seed.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Detail.create(	brand: "",
+# 								detail_id: "",
+# 								detail_title: "",
+# 								detail_price: ,
+# 								discount_group: ,
+# 								detail_weight: )
+Detail.create(	brand: "BMW",
+								detail_id: "123ABC",
+								detail_title: "Shurup",
+								detail_price: 23.16,
+								discount_group: "3",
+								detail_weight: 0.512)
+Detail.create(	brand: "Opel",
+								detail_id: "ABC123",
+								detail_title: "Bolt",
+								detail_price: 15.87,
+								discount_group: "A",
+								detail_weight: 0.656)
+Detail.create(	brand: "Honda",
+								detail_id: "456CDE",
+								detail_title: "Gayka",
+								detail_price: 9.23,
+								discount_group: "1",
+								detail_weight: 0.363)
+Detail.create(	brand: "Dodge",
+								detail_id: "CDE456",
+								detail_title: "Dverka",
+								detail_price: 65.87,
+								discount_group: "B",
+								detail_weight: 21.228)
