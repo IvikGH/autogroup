76f1a442e50c969a0b017c5acdbd8c80344ff722
Ivik <andoidxx@gmail.com>
2015-02-01 17:08:22 +0200

    Added Brand dropdown selection list

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -1,6 +1,9 @@
 class DetailsController < ApplicationController
   def index
-  	@details = Detail.where("detail_id LIKE ?", "%#{params[:q].upcase}%")
+  	@details = Detail.where(	"detail_id LIKE ? AND brand_id = ?",
+  														"%#{params[:q].upcase}%", 
+  														"#{params[:detail][:brand_id].to_i}"
+														)
   	# @details = Detail.where "detail_id LIKE :txt", txt: "%#{params[:q].upcase}%"
   end
 
