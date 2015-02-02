d052a25bebd6ac309d2a04f4892f6e1865a94e93
Ivik <andoidxx@gmail.com>
2015-02-02 18:42:23 +0200

    Add_quantity_to_line_items and cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ActiveRecord::Base
-	has_many	:line_items
+  has_many :line_items, dependent: :destroy
+
+  def add_detail(detail_id)
+    current_item = line_items.find_by(detail_id: detail_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(detail_id: detail_id)
+    end
+    current_item
+  end
 end
