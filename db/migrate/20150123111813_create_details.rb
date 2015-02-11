bb1cfe1f3481ec8d9f645c1501f0cb4dcf5e4780
Ivik <andoidxx@gmail.com>
2015-02-11 13:37:02 +0200

    Change migration becouse of import data error

--- db/migrate/20150123111813_create_details.rb
+++ db/migrate/20150123111813_create_details.rb
@@ -4,8 +4,8 @@ class CreateDetails < ActiveRecord::Migration
       t.integer :brand_id, index: true
     	t.string	:detail_id,       null: false
     	t.string	:detail_title
-    	t.decimal	:detail_price,    null: false, precision: 8, scale: 2
-    	t.string	:discount_group,   null: false
+    	t.decimal	:detail_price,    null: false, precision: 10, scale: 2
+    	t.string	:discount_group,  null: false
     	t.float		:detail_weight,   null: false, precision: 8, scale: 3
 
       t.timestamps                null: false
