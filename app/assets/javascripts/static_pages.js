1874ce182943a9bf6fb1e00f68cd09618a11f402
Ivik <andoidxx@gmail.com>
2015-02-28 00:21:20 +0200

    "Import files improvement with jQuery"

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,26 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+  var node1 = $('#DetailsImport .select-dropdown'),
+      button1 = $('#DetailsImport .submitButtn'),
+      node2 = $('#RabatImport .select-dropdown'),
+      button2 = $('#RabatImport .submitButtn');
+  button1.prop( "disabled", true );
+  button2.prop( "disabled", true );
+
+  node1.on ( 'mouseenter', function() {
+    button1
+      .prop( 'disabled', false )
+      .attr( 'class', "attention" );
+  })
+  button1.on( 'mouseenter', function() {
+    $(this)
+      .prop( "disabled", !node1.val() )
+      .removeClass( 'attention');
+  });
+
+  node2.on ( 'mouseenter', function() {
+    button2.prop( 'disabled', false );
+  })
+  button2.on( 'mouseenter', function() {
+    $(this).prop( "disabled", !node2.val() );
+  });
+});
