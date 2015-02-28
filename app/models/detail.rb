790e5ea1853b5d279602c72dd6c35db6f580b010
Ivik <andoidxx@gmail.com>
2015-02-28 16:22:44 +0200

    Real price value adds to :details calculated during details import

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -5,8 +5,14 @@ class Detail < ActiveRecord::Base
   acts_as_copy_target
 
   def self.import_scsv(file, brandID)
-    columns = [:detail_id, :detail_title, :detail_price, :discount_group, :detail_weight,:brand_id ]
+    columns = [:detail_id, :detail_title, :detail_price, :discount_group, :detail_weight,:brand_id, :real_price ]
     data = []
+    current_brand_rabats = Rabat.where( brand_id: brandID ).pluck(:title, :value)
+    current_brand_rabats_hash = Hash[*current_brand_rabats.flatten]
+    # current_brand_rabats.each do |hash|
+    #   rabat_hash << { hash[:title] => hash[:value] }
+    # end
+
     SmarterCSV.process( file.path,
                         { :chunk_size => 10000,
                           :col_sep => ";" ,
@@ -22,6 +28,10 @@ class Detail < ActiveRecord::Base
         hash[:detail_price] = 0.00 if hash[:detail_price] == "" || hash[:detail_price].nil?
         hash[:detail_weight] = 0.00 if hash[:detail_weight] == "" || hash[:detail_weight].nil?
         hash[:brand_id] = brandID
+        prise = BigDecimal(hash[:detail_price].to_s)
+        discount_string = hash[:discount_group].to_s
+        coefficient = current_brand_rabats_hash[discount_string]
+        hash[:real_price] = (prise * coefficient).round(2)
         data << hash.values
       end
       Detail.import(columns, data, validate: false)
--- db/migrate/20150226150929_add_real_price_to_details.rb
+++ db/migrate/20150226150929_add_real_price_to_details.rb
@@ -1,5 +1,5 @@
 class AddRealPriceToDetails < ActiveRecord::Migration
   def change
-    add_column :details, :real_price, :decimal, precision: 8, scale: 3
+    add_column :details, :real_price, :decimal, precision: 8, scale: 2
   end
 end
