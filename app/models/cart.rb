e444682efa6354f489edce6e90708d428e480084
Ivik <andoidxx@gmail.com>
2015-03-11 01:08:23 +0200

    Changed brands_margins hash in models.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,7 +10,38 @@ class Cart < ActiveRecord::Base
     else
       current_item = line_items.build(detail_id: detail_id)
       current_item.quantity = details_quantity
-      user.brands_margins = { quantity_to_cart: 1 }
+      user.brands_margins = { :quantity_to_cart => 1,
+                            "Bilstein" => 3,
+                            "BMW" => 3,
+                            "Chevrolet" => 3,
+                            "Chrysler" => 3,
+                            "Citroen" => 3,
+                            "Fiat" => 3,
+                            "Ford" => 3,
+                            "Honda" => 3,
+                            "Hyundai" => 3,
+                            "Infiniti" => 3,
+                            "Iveco" => 3,
+                            "Jaguar" => 3,
+                            "KIA" => 3,
+                            "Land Rover" => 3,
+                            "Mazda" => 3,
+                            "Mercedes Benz" => 3,
+                            "Mitsubishi" => 3,
+                            "Nissan" => 3,
+                            "Opel" => 3,
+                            "Peugeot" => 3,
+                            "Porsche" => 3,
+                            "Renault" => 3,
+                            "Seat" => 3,
+                            "Skoda" => 3,
+                            "Subaru" => 3,
+                            "Suzuki" => 3,
+                            "Toyota" => 3,
+                            "VAG" => 3,
+                            "Volvo" => 3,
+                            "VOTEX" => 3,
+                            "ZF" => 3 }
       user.save
     end
     current_item
@@ -31,8 +62,8 @@ class Cart < ActiveRecord::Base
     current_item
   end
 
-  def full_price(current_user_id)
-    line_items.to_a.sum { |item| item.total_price(current_user_id) }
+  def full_price(this_user_id)
+    line_items.to_a.sum { |item| item.total_price(this_user_id) }
   end
 
 end
