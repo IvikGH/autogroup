bf1b75299954ef06d9a17dc6450d46ebde42a6bf
Ivik <andoidxx@gmail.com>
2015-03-14 02:25:26 +0200

    Fixed ForbiddenAttributesError in ActiveAdmin

--- app/admin/detail.rb
+++ app/admin/detail.rb
@@ -1,5 +1,28 @@
 ActiveAdmin.register Detail do
 
+  index do
+    column "Brand" do |b|
+      Detail.find(b).brand.name
+    end
+    column :detail_id
+    column :detail_title
+    column :detail_price
+    column :discount_group
+    column :detail_weight
+    column :real_price
+    column :zusatz
+    column :ersatz
+    actions
+  end
+  permit_params :detail_id,
+                :detail_title,
+                :detail_price,
+                :discount_group,
+                :detail_weight,
+                :real_price,
+                :zusatz,
+                :ersatz,
+                brand_id: []
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
--- app/controllers/rabats_controller.rb
+++ app/controllers/rabats_controller.rb
@@ -3,4 +3,11 @@ class RabatsController < ApplicationController
     Rabat.import_scsv(params[:file], params[:detail][:brand_id].to_i)
     redirect_to static_pages_import_url, notice: "Data uploaded."
   end
+
+  private
+
+  def rabat_params
+      params.require(:rabat).permit(:brand_id, :title, :value)
+  end
+
 end
