69455af4a6386ec52c5e5217e034552bfc829abb
Ivik <andoidxx@gmail.com>
2015-03-22 15:09:21 +0200

    Updated user resource at ActiveAdmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register User do
 
   index do
+    column :activated
+    column :email
     column :name
     column :middle_name
     column :family_name
@@ -11,6 +13,7 @@ ActiveAdmin.register User do
     column :default_delivery_point_address
     column :default_additional_info
     column :margin
+    column :admin
     actions
   end
 
@@ -25,7 +28,10 @@ ActiveAdmin.register User do
                 :default_delivery_service,
                 :default_delivery_point_address,
                 :default_additional_info,
-                :brands_margins
+                :brands_margins,
+                :activated,
+                :admin
+
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
